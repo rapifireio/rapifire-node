58b1942a99704cbe4567889902b98f2b536efd30
prozz <pawel.rozynek@sentaca.com>
2015-12-08 12:12:03 +0100

    first version

---
+++ index.js
@@ -0,0 +1,113 @@
+"use strict";
+var WebSocket = require('ws');
+
+function buildInitPacket(config) {
+    return {
+        "operation": "init",
+        "data": {
+            "appId" : config.appId,
+            "authId" : config.authId,
+            "authKey" : config.authKey
+        }
+    };
+};
+
+function buildSubscribePacket(channel) {
+    return {
+        "operation": "subscribe",
+        "data": {
+            "channel": channel,
+            "interactive": false
+        }
+    };
+};
+
+function buildPublishPacket(channel, message) {
+    return {
+        "operation": "publish",
+        "data": {
+            "channel": channel,
+            "message": JSON.parse(message)
+        }
+    };
+};
+
+var Rapifire = function(authId, authToken, onConnect, onMessage, onError) {
+    var debug = false;
+    var config = {
+        url: 'ws://ws.rapifire.com/pubsub',
+        // internal app id for rapifire, dont change!
+        appId: '36799b71-c79f-405e-b345-cc5827d0e401',
+        authId: authId,
+        authKey: authToken
+    };
+
+    if (onConnect === undefined || onConnect === null) {
+        onConnect = function() {
+            console.log("(default onConnect handler) connected");
+        };
+    }
+    if (onMessage === undefined || onMessage === null) {
+        onMessage = function(channel, message, headers) {
+            console.log("(default onMessage handler) channel: %s, message: %s, headers: %s", channel, message, headers);
+        };
+    }
+
+    if (onError === undefined || onError === null) {
+        onError = function(message) {
+            console.log("(default onError handler) ERROR: " + message);
+        };
+    }
+
+    if (debug) {
+        console.log(config);
+    }
+
+    var self = this;
+    var ws = new WebSocket(config.url);
+
+    ws.on('open', function() {
+        if (debug) {
+            console.log("connected: %s", config.url);
+        }
+        ws.send(JSON.stringify(buildInitPacket(config)));
+        onConnect.apply(self);
+    });
+
+    ws.on('message', function(message) {
+        if (debug) {
+            console.log("received: %s", message);
+        }
+        var msg = JSON.parse(message);
+        if (msg.code !== undefined) {
+            onError.apply(self, [message]);
+            return;
+        }
+        onMessage.apply(self, [msg.channel, msg.message, msg.headers]);
+    });
+
+    this.subscribe = function (channel) {
+        var asJson = JSON.stringify(buildSubscribePacket(channel));
+        if (debug) {
+            console.log("subscribe: %s", asJson);
+        }
+        ws.send(asJson);
+        return this;
+    };
+
+    this.publish = function(channel, message) {
+        var asJson = JSON.stringify(buildPublishPacket(channel, JSON.stringify(message)));
+        if (debug) {
+            console.log("publish: %s", asJson);
+        }
+        ws.send(asJson);
+        return this;
+    };
+
+    this.disconnect = function() {
+        ws.disconnect();
+    };
+
+};
+
+module.exports = Rapifire;
