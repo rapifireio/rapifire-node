166eaf2b58da82fa2667af8c3ec8fcdb789858df
Maciej Lopacinski <hamsterready@gmail.com>
2015-12-17 14:23:22 +0100

    M-x untabify, M-x indent-buffer

--- example/coffee.js
+++ example/coffee.js
@@ -1,10 +1,12 @@
 var Rapifire = require('../index.js');
 
-// Data and Commands channels as well as Auth ID and Auth Token can be found in top right
-// corner of your thing details page in 'API Access Keys and Channel Names' section.
+// Data and Commands channels as well as Auth ID and Auth Token can be
+// found in top right corner of your thing details page in 'API Access
+// Keys and Channel Names' section.
 
-// Sending messages to Rapifire (SenML required).
-// For details about SenML see our blog post here: http://blog.rapifire.com/introduction-to-senml/
+// Sending messages to Rapifire (SenML required). For details about
+// SenML see our blog post here:
+// http://blog.rapifire.com/introduction-to-senml/
 var dataChannel = "/mRRRRRRRRme/data";
 
 // Reading commands from office (any format).
@@ -12,17 +14,17 @@ var commandsChannel = "/mRRRRRRRRme/commands";
 
 // SenML formatted message.
 function machineStatus(status) {
-    return {
-        "bn":"/coffeeMachine/",
-        "e":[{"n":"status", "sv": status}]
-    };
+  return {
+    "bn":"/coffeeMachine/",
+    "e":[{"n":"status", "sv": status}]
+  };
 }
 
 // SenML formatted message.
 function orderReady(coffeeType) {
-    return {
-        "bn":"/coffeeMachine/",
-        "e":[{"n":"order", "sv": coffeeType}]};
+  return {
+    "bn":"/coffeeMachine/",
+    "e":[{"n":"order", "sv": coffeeType}]};
 }
 
 // Is coffee machine working or in maintenance mode?
@@ -30,32 +32,35 @@ var operational = true;
 
 // Coffee machine behaviour when connected to Rapifire.
 function onConnect() {
-    console.log("coffee machine operational.");
-    this.subscribe(commandsChannel);
-    this.publish(dataChannel, machineStatus("on"));
+  console.log("coffee machine operational.");
+  this.subscribe(commandsChannel);
+  this.publish(dataChannel, machineStatus("on"));
 }
 
-// Coffee machine behaviour when message is read from Rapifire (from commands channel, that we subscribed to in onConnect).
+// Coffee machine behaviour when message is read from Rapifire (from
+// commands channel, that we subscribed to in onConnect).
 function onMessage(channel, message, headers) {
-    if (operational) {
-        console.log("preparing " + message);
-        // brew it here!
-        console.log("order ready!");
-        this.publish(dataChannel, orderReady(message));
-    } else {
-        console.log("cannot prepare " + message + " due to ongoing maintenance");
-    }
+  if (operational) {
+    console.log("preparing " + message);
+    // brew it here!
+    console.log("order ready!");
+    this.publish(dataChannel, orderReady(message));
+  } else {
+    console.log("cannot prepare " + message + " due to ongoing maintenance");
+  }
 }
 
-// Connect to Rapifire with your Thing's Auth ID and Auth Token, pass proper handlers.
-// Replace Auth ID and Auth Token with data from your thing details page.
+// Connect to Rapifire with your Thing's Auth ID and Auth Token, pass
+// proper handlers. Replace Auth ID and Auth Token with data from your
+// thing details page.
 var coffeeMachine = new Rapifire('pkU6b-Tc420qFKPdwHJn8L2rWFA=','qFQhkjetZArC', onConnect, onMessage);
 
-// After connecting, we do inform Rapifire and all subscribers about maintenance of our machine by sending proper status message.
+// After connecting, we do inform Rapifire and all subscribers about
+// maintenance of our machine by sending proper status message.
 setInterval(function() {
-    // check if maintenance status needs change
-    if (!operational || Math.random() > 0.8) {
-        operational = !operational;
-        coffeeMachine.publish(dataChannel, machineStatus(operational ? "on" : "off"));
-    }
+  // check if maintenance status needs change
+  if (!operational || Math.random() > 0.8) {
+    operational = !operational;
+    coffeeMachine.publish(dataChannel, machineStatus(operational ? "on" : "off"));
+  }
 }, 1000);
--- index.js
+++ index.js
@@ -2,107 +2,121 @@
 var WebSocket = require('ws');
 
 function buildInitPacket(config) {
-    return {
-        "operation": "init",
-        "data": {
-            "authId" : config.authId,
-            "authKey" : config.authKey
-        }
-    };
+  return {
+    "operation": "init",
+    "data": {
+      "authId" : config.authId,
+      "authKey" : config.authKey
+    }
+  };
 };
 
 function buildSubscribePacket(channel) {
-    return {
-        "operation": "subscribe",
-        "data": {
-            "channel": channel
-        }
-    };
+  return {
+    "operation": "subscribe",
+    "data": {
+      "channel": channel
+    }
+  };
 };
 
 function buildPublishPacket(channel, message) {
-    return {
-        "operation": "publish",
-        "data": {
-            "channel": channel,
-            "message": JSON.parse(message)
-        }
-    };
+  return {
+    "operation": "publish",
+    "data": {
+      "channel": channel,
+      "message": JSON.parse(message)
+    }
+  };
 };
 
+/**
+ * RAPIFIRE Client Constructor
+ * 
+ * @constructor 
+ * 
+ * @param {string} authId - client authentication id, thing id, user
+ * id or developer auth id
+ * 
+ * @param {string} authToken - client authentication token,
+ * @param {function} onConnect - callback invoked on connect event
+ * @param {function} onMessage - callback invoked on each received message
+ * @param {function} onError - callback invoked in case of error
+ *
+ */
 var Rapifire = function(authId, authToken, onConnect, onMessage, onError) {
-    var debug = false;
-    var config = {
-        url: 'ws://ws.rapifire.com/pubsub',
-        authId: authId,
-        authKey: authToken
+  var debug = false;
+  var config = {
+    url: 'ws://ws.rapifire.com/pubsub',
+    authId: authId,
+    authKey: authToken
+  };
+
+  if (onConnect === undefined || onConnect === null) {
+    onConnect = function() {
+      console.log("(default onConnect handler) connected");
     };
+  }
+  if (onMessage === undefined || onMessage === null) {
+    onMessage = function(channel, message, headers) {
+      console.log("(default onMessage handler) channel: %s, message: %s, headers: %s", channel, message, headers);
+    };
+  }
 
-    if (onConnect === undefined || onConnect === null) {
-        onConnect = function() {
-            console.log("(default onConnect handler) connected");
-        };
-    }
-    if (onMessage === undefined || onMessage === null) {
-        onMessage = function(channel, message, headers) {
-            console.log("(default onMessage handler) channel: %s, message: %s, headers: %s", channel, message, headers);
-        };
-    }
+  if (onError === undefined || onError === null) {
+    onError = function(message) {
+      console.log("(default onError handler) ERROR: " + message);
+    };
+  }
 
-    if (onError === undefined || onError === null) {
-        onError = function(message) {
-            console.log("(default onError handler) ERROR: " + message);
-        };
-    }
+  if (debug) {
+    console.log(config);
+  }
 
+  var self = this;
+  var ws = new WebSocket(config.url);
+
+  ws.on('open', function() {
     if (debug) {
-        console.log(config);
+      console.log("connected: %s", config.url);
     }
+    ws.send(JSON.stringify(buildInitPacket(config)));
+    onConnect.apply(self);
+  });
 
-    var self = this;
-    var ws = new WebSocket(config.url);
+  ws.on('message', function(message) {
+    if (debug) {
+      console.log("received: %s", message);
+    }
+    var msg = JSON.parse(message);
+    if (msg.code !== undefined) {
+      onError.apply(self, [message]);
+      return;
+    }
+    onMessage.apply(self, [msg.channel, msg.message, msg.headers]);
+  });
 
-    ws.on('open', function() {
-        if (debug) {
-            console.log("connected: %s", config.url);
-        }
-        ws.send(JSON.stringify(buildInitPacket(config)));
-        onConnect.apply(self);
-    });
+  this.subscribe = function (channel) {
+    var asJson = JSON.stringify(buildSubscribePacket(channel));
+    if (debug) {
+      console.log("subscribe: %s", asJson);
+    }
+    ws.send(asJson);
+    return this;
+  };
 
-    ws.on('message', function(message) {
-        if (debug) {
-            console.log("received: %s", message);
-        }
-        var msg = JSON.parse(message);
-        if (msg.code !== undefined) {
-            onError.apply(self, [message]);
-            return;
-        }
-        onMessage.apply(self, [msg.channel, msg.message, msg.headers]);
-    });
+  this.publish = function(channel, message) {
+    var asJson = JSON.stringify(buildPublishPacket(channel, JSON.stringify(message)));
+    if (debug) {
+      console.log("publish: %s", asJson);
+    }
+    ws.send(asJson);
+    return this;
+  };
 
-    this.subscribe = function (channel) {
-        var asJson = JSON.stringify(buildSubscribePacket(channel));
-        if (debug) {
-            console.log("subscribe: %s", asJson);
-        }
-        ws.send(asJson);
-        return this;
-    };
-
-    this.publish = function(channel, message) {
-        var asJson = JSON.stringify(buildPublishPacket(channel, JSON.stringify(message)));
-        if (debug) {
-            console.log("publish: %s", asJson);
-        }
-        ws.send(asJson);
-        return this;
-    };
-
-    this.disconnect = function() {
-        ws.disconnect();
-    };
+  this.disconnect = function() {
+    ws.disconnect();
+  };
 
 };
 
